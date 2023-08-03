d3b7bf49960106db97eeb8c539812ab9eb016082
tannguyendinh <dinhtannguyen9x@gmail.com>
2023-08-03 19:34:15 +0700

    test

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-3"
           >
-            Sign In
+            Đăng nhập 
           </button>
           <button
             data-collapse-toggle="navbar-sticky"
