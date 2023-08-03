8c99e9da577173788c7d604067b5ac639bc14cf7
tannguyendinh <dinhtannguyen9x@gmail.com>
2023-08-03 19:37:37 +0700

    test lại

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -32,7 +32,8 @@ const Header = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
           >
-            Sign Up
+            đăng kí
+            
           </button>
           <button
             type="button"
